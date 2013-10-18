478c953e956a7dd687004937ac7642d861086592
Hero <nguyendatnq@gmail.com>
2013-10-18 22:46:36 +0700

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,7 +1,20 @@
 package com.git.java;
 
 public class Hero {
+	private static Hero INSTANCE;
 	public Hero() {
 
+	}
+
+	public static Hero getInstance() {
+		if (INSTANCE == null) {
+			synchronze(Hero.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new Hero();
+				}
+			}
+		}
+
+		return INSTANCE;
 	}
 }
